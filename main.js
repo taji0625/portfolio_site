3e51ad3b720f91dead82337e4d7dc25d9b4d6341
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-10-14 18:28:58 +0900

    ヘッダーの作成

---
+++ main.js
@@ -0,0 +1,18 @@
+(function($) {
+  var $nav   = $('#navArea');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask');
+  var open   = 'open'; 
+
+  $btn.on( 'click', function() {
+    if ( ! $nav.hasClass( 'open' ) ) {
+      $nav.addClass( 'open' );
+    } else {
+      $nav.removeClass( 'open' );
+    }
+  });
+
+  $mask.on('click', function() {
+    $nav.removeClass( 'open' );
+  });
+} )(jQuery);
