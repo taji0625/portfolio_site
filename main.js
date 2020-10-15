1bea49272393247e736539cef343842fc20ff7a3
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-10-15 17:39:29 +0900

    about_meのpc版まで

--- main.js
+++ main.js
@@ -16,3 +16,19 @@
     $nav.removeClass( 'open' );
   });
 } )(jQuery);
+
+var _window = $(window),
+  _header = $('header'),
+  heroBottom;
+ 
+_window.on('scroll',function(){     
+  heroBottom = $('.top-wrapper').height();
+  if(_window.scrollTop() > heroBottom){
+    _header.addClass('fixed');   
+  }
+  else{
+    _header.removeClass('fixed');   
+  }
+});
+ 
+_window.trigger('scroll');
